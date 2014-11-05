489d3c1b722182740026f4e3d28160692bdf76e8
Jimmy Gong <gong.jim@gmail.com>
2014-11-05 15:13:45 -0800

    Complete rook challenge

--- src/solvers.js
+++ src/solvers.js
@@ -14,19 +14,46 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n:n});
+  var nQueens = 0;
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  for (var r = 0; r < n; r++) {
+    for (var c = 0; c < n; c++) {
+      solution.togglePiece(r, c);
+      nQueens++;
+      if (solution.hasAnyRooksConflicts()) {
+        solution.togglePiece(r, c);
+        nQueens--;
+      }
+    }
+  }
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return solution.rows();
 };
-
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  //rowIndex = rows below taken row
+  var solutionCount = 0;
+  function subBoard (rowIndex, board) {
+    if(rowIndex === n - 1) {
+       solutionCount++;
+       return;
+    }
+    var oldBoard = board.rows();
+    var tempBoard = new Board(oldBoard);
+    for (var c = 0; c < n; c++) {
+      tempBoard.togglePiece(rowIndex, c);
+      if (!tempBoard.hasAnyRooksConflicts()) {
+        rowIndex++;
+        subBoard(rowIndex, tempBoard);
+        rowIndex--;
+      }
+      tempBoard.togglePiece(rowIndex, c);
+    }
+  }
+  subBoard(0, new Board({n:n}));
   return solutionCount;
 };
 
